b0524a3442c174874f487ae1897c4ffee71a25e6
Lenaben19 <lenalena112019@gmail.com>
2024-11-22 18:29:05 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("# search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener(" submit", handleSearchSubmit);
+
 
 
 
