80e8e93c85c1ea906cc670ea2ed648c8e96ac104
Lenaben19 <lenalena112019@gmail.com>
2024-11-22 21:10:51 -0400

    Added API key

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+let apiKey = "069ec8tb83a25aad423c8d02ebfb0do6 ";
+let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"';
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
