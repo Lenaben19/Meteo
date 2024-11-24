cef287363f9182bd5257e04dec63d7d0a288f481
Lenaben19 <lenalena112019@gmail.com>
2024-11-24 19:44:09 -0400

    Added the search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-function searchCity(city) {
-let apiKey = "069ec8tb83a25aad423c8d02ebfb0do6 ";
-let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"';
-  console.log(apiUrl);
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+     let cityElement = document.querySelector("#city");
+     cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function searchCity(city) {
+let apiKey = "069ec8tb83a25aad423c8d02ebfb0do6";
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
@@ -16,6 +23,6 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 
-
+searchCity("Halifax");
 
 
